2e58887189a475c087777caf878d2fa4d73c5a6c
Geord <Geord@DESKTOP-G489K85>
2025-11-11 20:22:46 +0200

    Добавьте файлы проекта.

---
+++ koledzDz16/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace koledzDz16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[] array = new int[12];
+            //int misats1 = 0, misats2 = 0;
+            //for (int i = 0; i < array.Length; i++)
+            //{
+            //    Console.Write("Ведіть пробуток фирми за " + (i + 1) + " місяць: ");
+            //    array[i] = Convert.ToInt32(Console.ReadLine());
+            //}
+            //Console.Write("ведіть місяць від якого будемо рахувати ");
+            //misats1= Convert.ToInt32(Console.ReadLine());
+            //Console.Write("ведіть місяць до якого будемо рахувати ");
+            //misats2= Convert.ToInt32(Console.ReadLine());
+            //int min=2147483647;
+            //int max=-2147483647;
+            //int minIndex = 0, maxIndex = 0;
+            //for (int i = misats1-1; i < misats2; i++)
+            //{
+            //    if (array[i] < min)
+            //    {
+            //        min = array[i];
+            //        minIndex = i;
+            //    }
+            //    if (array[i] > max)
+            //    {
+            //        max = array[i];
+            //        maxIndex = i;
+            //    }
+            //}
+            //Console.WriteLine();
+            //Console.WriteLine("Місяць з найменшим прибутком у діапазоні " + (minIndex + 1));
+            //Console.WriteLine("Місяць з найбільшим прибутком у діапазоні " + (maxIndex + 1));
+
+            int[] array = new int[10];
+            int sum = 0;
+            Random rand = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rand.Next(-10, 10);
+            }
+            for (int i = 0; i < array.Length; i++)
+                {
+                if (array[i] < 0)
+                {
+                    sum += array[i];
+                }
+            }
+            Console.WriteLine("Сума від'ємних елементів: " + sum);
+            Console.WriteLine("Масив: " + string.Join(", ", array));
+        }
+    }
+}
---
+++ koledzDz16/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("koledzDz16")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("koledzDz16")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("d8308d5e-fbfc-4944-9fa8-ad56d6446fd0")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
